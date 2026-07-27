806c9cd83ac278127a5050144825ef476c78f2e9
MelindaRespeto <145196890+MelindaRespeto@users.noreply.github.com>
2026-07-27 09:01:14 +0800

    add

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -2,3 +2,22 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 
 const myCheckout = new CheckoutProcess("so-cart", "#order-summary");
 myCheckout.init();
+
+document.querySelector("#checkoutSubmit").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const form = document.querySelector("#checkout-form");
+    const isValid = form.checkValidity();
+
+    if (!isValid) {
+        form.reportValidity();
+        return;
+    }
+
+    try {
+        await myCheckout.checkout(form);
+        window.location.assign("./success.html");
+    } catch (err) {
+        console.log("Checkout failed:", err);
+    }
+});
